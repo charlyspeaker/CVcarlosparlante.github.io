3e9526c1aebced655715f97058d34ae205072b6c
carlos <123005808+charlyspeaker@users.noreply.github.com>
2023-02-05 20:45:49 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,61 @@
+function mOver1(obj) {
+    obj.innerHTML = "click Aqui!"
+}
+
+function mOut1(obj) {
+    obj.innerHTML = "Sobre MI"
+}
+
+function mOut2(obj) {
+    obj.innerHTML = "Mi Direccion"
+}
+
+function mOut3(obj) {
+    obj.innerHTML = "Mi Foto"
+}
+function mOut4(obj) {
+    obj.innerHTML = "Contacto"
+}
+
+function bigImg1(x) {
+    document.getElementById("yo").style.color = "red";
+    document.getElementById('yo').style.fontSize='50px';
+}
+  
+function normalImg1(x) {
+    document.getElementById("yo").style.color = "white";
+    document.getElementById('yo').style.fontSize='26px'
+}
+
+function bigImg2(x) {
+    document.getElementById("casa").style.color = "red";
+    document.getElementById('casa').style.fontSize='50px'
+}
+  
+function normalImg2(x) {
+    document.getElementById("casa").style.color = "white";
+    document.getElementById('casa').style.fontSize='26px'
+}
+
+function bigImg3(x) {
+    document.getElementById("miFoto").style.color = "red";
+    document.getElementById('miFoto').style.fontSize='50px'
+    
+}
+  
+function normalImg3(x) {
+    document.getElementById("miFoto").style.color = "white";
+    document.getElementById('miFoto').style.fontSize='26px'
+}
+function bigImg4(x) {
+    document.getElementById("Mail").style.color = "red";
+    document.getElementById('Mail').style.fontSize='50px'
+    
+}
+function normalImg4(x) {
+    document.getElementById("Mail").style.color = "white";
+    document.getElementById('Mail').style.fontSize='26px';
+}
+
+
+
